7336f250ed2ab3744ea8c8033e32091c42d75c15
kohmiura <kohei.2126@gmail.com>
2021-03-29 03:04:43 +0900

    rm menu

--- src/components/AmongUsTool/Header.tsx
+++ src/components/AmongUsTool/Header.tsx
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
-import FaceImage from "../../assets/image/Face.png";
-import {
-  Menu,
-  Typography,
-  AppBar,
-  Toolbar,
-  IconButton,
-  MenuItem,
-  Link,
-  Button,
-} from "@material-ui/core";
+import { Typography, AppBar, Toolbar, Button } from "@material-ui/core";
 import ExportButton from "./ExportButton";
 import ImportButton from "./ImportButton";
 import Icon from "../../assets/image/Icon.png";
-import MoreVertIcon from "../../assets/icons/MoreVert.svg";
 import { usageOperations } from "../../modules/usage";
 import { useDispatch } from "react-redux";
 
@@ -23,14 +12,6 @@ const StyledAppBar = styled(AppBar)``;
 const StyledToolbar = styled(Toolbar)`
   flex-wrap: wrap;
   padding-right: ${(props) => props.theme.spacing(1)};
-`;
-const StyledTwitterImage = styled("img").attrs({ src: FaceImage })`
-  width: 32px;
-  height: 32px;
-  border-radius: 100px;
-`;
-const TwitterName = styled(Typography)`
-  line-height: 1;
 `;
 const Img = styled("img")`
   height: 40px;
@@ -53,24 +34,8 @@ const Buttons = styled("span")`
   margin-right: ${(props) => props.theme.spacing(1)};
 `;
 
-const TwitterArea = styled("span")`
-  display: flex;
-  align-items: flex-end;
-`;
-
-const StyledMoreVertIcon = styled(MoreVertIcon)`
-  fill: ${(props) => props.theme.palette.text.secondary};
-`;
 const Header: React.FunctionComponent = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
   const dispatch = useDispatch();
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   const openUsage = () => {
     dispatch(usageOperations.open());
   };
@@ -88,48 +53,6 @@ const Header: React.FunctionComponent = () => {
           <ExportButton />
           <ImportButton />
         </Buttons>
-        <div>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
-          >
-            <StyledMoreVertIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={open}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>
-              <Link
-                underline="none"
-                href="https://twitter.com/Warabimochi_m"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Typography variant="body2" color="textPrimary">
-                  作者
-                </Typography>
-                <TwitterArea>
-                  <StyledTwitterImage />
-                  <TwitterName>@Warabimochi_m</TwitterName>
-                </TwitterArea>
-              </Link>
-            </MenuItem>
-          </Menu>
-        </div>
       </StyledToolbar>
     </StyledAppBar>
   );
